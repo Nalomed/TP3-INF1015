e00dbf1ef9d3400b6f97eca49fae9d4e5af6a498
unknown <nazim.bertouche@polymtl.ca>
2022-10-25 21:03:13 -0400

    ajout 2&4

--- TD2-A22-Solutionnaire/Liste.hpp
+++ TD2-A22-Solutionnaire/Liste.hpp
@@ -12,21 +12,21 @@ class Liste {
 public:
 	Liste() = default;
 	Liste() { nElements_ = 0; capacite_ = 1; elements_ = make_unique<shared_ptr<T>[]>(capacite_); }
-	
 
 	T ajouter(shared_ptr <T> d);
 	T retirer(const T aRetirer);
 	T afficher() const;
+	T ajouterElement(string element);
+	T operator[] (position)
 
-	
 	shared_ptr<T>& operator[](int index) { return elements_[index]; }
 
 	shared_ptr<T> trouverElement(const function<bool(shared_ptr<T>)> critere);
 
 	T changerCapacite(T nouvelleCapacite);
+	int nElements_ = 0;
 private:
 	  
-	T nElements_ = 0;
 	T capacite_ = 0;  // Pas besoin de déclarer explicitement un corps de constructeur avec ces initialisations.
 	unique_ptr<shared_ptr<T>[]> elements_ = nullptr;
 };
@@ -37,6 +37,22 @@ T Liste<T>::afficher() const
 {
 	for (const T* d : enSpan())
 		d->afficher();
+}
+
+template<typename T>
+T Liste<T>::ajouterElement(element)
+{
+	Liste Liste.insert(element, nElements_);
+	nElements_++;
+}
+
+template<typename T>
+T Liste<T>:: operator[](int posElement)
+{
+	for (int i = 0; i < T.capacite_; i++) {
+		if (i == posElement)
+			return  afficher(posElement);
+	}
 }
 
 template<typename T>
@@ -68,6 +84,12 @@ T Liste<T>::retirer(const T aRetirer)
 			nElements_--;
 		}
 	}
+}
+
+template<typename T>
+inline shared_ptr<T> Liste<T>::trouverElement(const function<bool(shared_ptr<T>)> critere)
+{
+	return shared_ptr<T>();
 }
 
 template<typename T>
